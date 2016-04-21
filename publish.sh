b3da6e8fd6c28b3c42d55e792cefba86ecbd9d43
peter <peter@europa>
2016-04-21 12:24:26 +0100

    Remove env test as not available to jenkins user

--- publish.sh
+++ publish.sh
@@ -1,20 +1,17 @@
 #!/bin/ash
 
-if [ -n "$DOCKER_USER" ]
-then
-    for i in \
-        area51/alpine \
-	area51/java:serverjre-8 \
-	area51/java:jre-8 \
-	area51/java:jdk-8 \
-	area51/docker-client:latest \
-	area51/jenkins-slave:latest \
-	area51/jenkins:latest \
-	area51/ubuntu-dev:latest \
-	area51/nexus:latest \
-	area51/rabbitmq:latest \
-	area51/postgis:latest
-    do
-	docker push $i
-    done
-fi
+for i in \
+    area51/alpine \
+    area51/java:serverjre-8 \
+    area51/java:jre-8 \
+    area51/java:jdk-8 \
+    area51/docker-client:latest \
+    area51/jenkins-slave:latest \
+    area51/jenkins:latest \
+    area51/ubuntu-dev:latest \
+    area51/nexus:latest \
+    area51/rabbitmq:latest \
+    area51/postgis:latest
+do
+    docker push $i
+done
