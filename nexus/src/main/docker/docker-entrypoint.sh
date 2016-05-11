6da1984592ea0ce66e758c2e4540f9de11f3b9aa
peter <peter@europa>
2016-05-11 20:29:29 +0100

    Add option to enable IPv6 with Nexus 3

--- nexus/src/main/docker/docker-entrypoint.sh
+++ nexus/src/main/docker/docker-entrypoint.sh
@@ -5,4 +5,12 @@ sed -i \
     -e "s|nexus-context-path=/|nexus-context-path=${CONTEXT_PATH}|g" \
     /opt/sonatype/nexus/etc/org.sonatype.nexus.cfg
 
+# Enable IPv6 support?
+if [ -n "$IPv6" ]
+then
+    VMOPTS=/opt/sonatype/nexus/bin/nexus.vmoptions
+    cp $VMOPTS /tmp/.vm
+    grep -v "java.net.preferIPv4Stack" /tmp/.vm >$VMOPTS
+fi
+
 exec /opt/sonatype/nexus/bin/nexus run
