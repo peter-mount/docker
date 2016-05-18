90942aae9d35926dc918fba71cae627e05359c4f
peter <peter@europa>
2016-05-18 11:47:23 +0100

    Hack to attempt (working so far) to fix JENKINS-32542

--- jenkins/src/main/scripts/config-jenkins.sh
+++ jenkins/src/main/scripts/config-jenkins.sh
@@ -16,3 +16,11 @@ then
     adduser root "$gn"
     adduser jenkins "$gn"
 fi
+
+# Bug JENKINS-32542 I found I hit this bug suddenly then found JENKINS-29674 mentioned
+# about the loopback used by maven and the slave. Adding this seems (as I write this)
+# to fix this bug.
+#
+# Now we have to do this at container startup as the host file is replaced by docker
+# everytime the container is started.
+echo "127.0.0.1 dockerhost" >>/etc/hosts
