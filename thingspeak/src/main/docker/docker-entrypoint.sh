9e55ff3d0c4f82b4f983708bbba437dd01c6886f
peter <peter@europa>
2016-05-25 21:18:23 +0100

    Switch to using mysql for thingspeak

--- thingspeak/src/main/docker/docker-entrypoint.sh
+++ thingspeak/src/main/docker/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 cd /opt/thingspeak
 
-DB_NAME=development
 echo "Initialising environment"
 (
-    echo "${DB_NAME}:"
-    echo "  adapter: postgresql"
-    echo "  encoding: unicode"
-    echo "  database: ${env}"
+    echo "development:"
+    echo "  adapter: mysql2"
+    echo "  encoding: utf8"
+    echo "  reconnect: true"
+    echo "  database: ${DB_NAME}"
     echo "  pool: ${DB_POOL}"
     echo "  username: ${DB_USER}"
     echo "  password: ${DB_PASSWORD}"
@@ -17,21 +17,5 @@ echo "Initialising environment"
     echo "  min_messages: WARNING"
 ) >config/database.yml
 
-echo "pgpass"
-(
-    for DB in postgres ${DB_NAME}
-    do
-        echo "${DB_HOST}:${DB_PORT}:${DB}:${DB_USER}:${DB_PASSWORD}"
-    done
-) >~/.pgpass
-chmod 0400 ~/.pgpass
-
-echo "Checking database"
-psql -h ${DB_HOST} -U ${DB_USER} ${DB_NAME} -c "" || (
-    echo "Initialising database"
-    rake db:create
-    rake db:schema:load
-)
-
 echo "Booting thingspeak"
-exec bundle exec rails server -p ${HTTP_PORT}
+exec bundle exec rails server -p ${HTTP_PORT} 2>&1 >/dev/null
